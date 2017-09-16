f608d2b7fc8a2869fc31626026cfb13436a7397c
ror-shubham <ror-shubham@github.com>
2017-09-17 05:13:15 +0530

    added dummy Verify function

--- smartContracts/contracts/Block.sol
+++ smartContracts/contracts/Block.sol
@@ -23,22 +23,26 @@ contract Block{
         //bytes32 telephone;
     }
     
-    //just for testing
-    function getCerti(address _address) constant returns (address _add){
-        var p = Users[_address].certificates[0].issuer.publickey;
-        return p;
+    //just check for address of issuer and certiname for now
+    function Verify(address _user, address _issuer, bytes32 _certName) constant returns (bool success){
+        var certificates = Users[_user].certificates;
+        for (uint i=0; i<certificates.length; i++){
+            if ((certificates[i].issuer.publickey==_issuer) && (certificates[i].certi_name == _certName)){
+                return true;
+            }
+        }
+        return false;
     }
-    
 
-    function issueCertificate(address _recipient, bytes32 _certi_name) returns (bool _bool){
+    function issueCertificate(address _recipient, bytes32 _certi_name) returns (bool sucess){
         User storage u = Users[_recipient];
         Certificate memory c;
         c.issuer.publickey = msg.sender;
-]        c.certi_name = _certi_name;
+        c.certi_name = _certi_name;
         c.issuedOn = now;
         var p = u.certificates;
         p.push(c);
-        return true ;
+        return true;
     }
     
 }
